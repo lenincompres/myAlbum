f1eba293e448ab6c2985b9a38a9be4464f2b7f15
lenincompres <lenincompres@gmail.com>
2017-12-16 08:23:43 -0500

    Removing JQuery

--- script.js
+++ script.js
@@ -1,134 +1,146 @@
 /**
  * @file handling a playlist
  * @author Lenin Compres <lenincompres@gmail.com>
- * @version 0.1
+ * @version 0.2
  **/
 
-'use strict';
 
 (function() {
 
-  //var audio = document.getElementById('audio');
+  "use strict";
+
   var audioFiles = [];
-  var tracklist = $('#tracklist li');
-  var audioBox = $('#audio').first(),
-    audio = audioBox[0],
-    loader = $('#loader').first(),
-    lyricsBox = $('#lyrics').first(),
-    lyrics = $('#lyrics').find('div').first(),
-    videoBox = $("#video").first();
-  var stopBtn = $('#stop').first(),
-    infoBtn = $('#info').first(),
-    closeBtn = $('#close').first();
+  var tracklist = document.querySelectorAll('#tracklist li');
+  var audio = document.querySelector('#audio'),
+    //loader = $('#loader').first(),
+    lyricsBox = document.querySelector('#lyrics'),
+    videoBox = document.querySelector("#video"),
+    stopBtn = document.querySelector('#stop'),
+    infoBtn = document.querySelector('#info'),
+    closeBtn = document.querySelector('#close');
   var activeSong = null;
-  var videoH = videoBox.width() * 9 / 16;
+  var videoH = videoBox.offsetWidth * 9 / 16;
 
   /* set up */
 
   //sets video for css to animate to correct aspect ratio
-  videoBox.attr('height', videoH);
+  videoBox.style.height = videoH + 'px';
 
   //makes sure the lyrics page has the height of the tracklist
-  lyricsBox.height($('main').height());
+  lyricsBox.style.height = document.querySelector('#tracklist').offsetHeight + 'px';
 
   //adds the index number in front of each track on the list
-  var index = 1
-  tracklist.each(function() {
-    let song = $(this);
-    let f = 'songs/' + song.find('a').first().text().toLocaleLowerCase() + '.mp3';
+  var index = 1;
+  for (let i = 0; i < tracklist.length; i++) {
+    let song = tracklist[i];
+    let f = 'songs/' + song.querySelector('a').innerHTML.toLocaleLowerCase() + '.mp3';
     audioFiles.push(f);
 
     //preps the song link
-    song.prepend('<span>' + (index > 9 ? "" : "0") + index + ' - </span>').attr("index", index++);//.addClass("disabled");
-  });
-/*
-  //preloads songs
-  $(document).ready(function() {
-    let i = 0;
-    while (audioFiles[i]) {
-      var a = new Audio();
-      let n = i;
-      a.addEventListener('canplaythrough', function() {
-        audioLoaded(parseInt(n));
-      }, false);
-      a.addEventListener('load', function() {
-        audioLoaded(parseInt(n));
-      }, false);
-      a.preload = 'auto';
-      a.src = audioFiles[n];
-      i++;
-    }
-  });
+    let span = document.createElement("span");
+    span.innerHTML = (index > 9 ? "" : "0") + index + ' -';
+    song.prepend(span);
+    song.setAttribute("index", index++);
+    //song.classList.add("disabled");
+    song.addEventListener("click", selectSong);
+  }
 
-  //loading indicator
-  function audioLoaded(n) {
-    let song = tracklist.eq(n);
-    if (song.hasClass('disabled')) {
-      let p = (index - 1) / tracklist.length;
-      loader.width(100 * p + '%').css('opacity', p);
-      tracklist.eq(n).removeClass('disabled');
-      index--;
-    }
-  }*/
-
-  /* controls */
-
-  tracklist.click(function() {
-    let song = $(this);
-    if (!song.hasClass('active') && !song.hasClass('disabled')) {
+  function selectSong(e) {
+    let song = e.target;
+    if (!song.classList.contains('active') && !song.classList.contains('disabled')) {
 
       //selects and updates active song
       if (activeSong !== null) {
-        activeSong.removeClass('active');
+        activeSong.classList.remove('active');
       }
-      activeSong = song.addClass('active').append(stopBtn.detach()).append(infoBtn.detach());
-      let title = song.find('a').first().text();
+      song.classList.add('active');
+      activeSong = song;
+      song.append(stopBtn);
+      song.append(infoBtn);
 
       //plays song
       audio.pause();
-      audio.src = audioFiles[activeSong.attr("index") - 1];
+      audio.src = audioFiles[activeSong.getAttribute("index") - 1];
       audio.addEventListener('ended', audioEnded);
       audio.play();
-      audioBox.show();
-      videoBox.attr('height', 0);
+      audio.style.display = "block";
+      videoBox.style.height = 0;
 
       //loads lyrics
-      lyrics.load('lyrics/' + title.toLowerCase().replace(/ /g, "%20") + '.html', function(data, status) {
-        if (status === 'error') {
-          infoBtn.hide();
-          lyricsBox.removeClass('active');
-        } else {
+      let req = new XMLHttpRequest();
+      let filename = song.querySelector('a').innerHTML.toLowerCase().replace(/ /g, "%20");
+      req.open('GET', 'lyrics/' + filename + '.html');
+      req.onreadystatechange = function() {
+        if (req.readyState === 4 && req.status === 200) {
+          lyricsBox.querySelector('div').innerHTML = req.responseText;
+          infoBtn.style.display = "block";
           infoBtn.click();
+        } else {
+          infoBtn.style.display = "none";
+          lyricsBox.classList.remove('active');
         }
-      });
+      };
+      req.send();
     }
-  });
+  }
+
+  /*
+    //preloads songs
+    $(document).ready(function() {
+      let i = 0;
+      while (audioFiles[i]) {
+        var a = new Audio();
+        let n = i;
+        a.addEventListener('canplaythrough', function() {
+          audioLoaded(parseInt(n));
+        }, false);
+        a.addEventListener('load', function() {
+          audioLoaded(parseInt(n));
+        }, false);
+        a.preload = 'auto';
+        a.src = audioFiles[n];
+        i++;
+      }
+    });
+
+    //loading indicator
+    function audioLoaded(n) {
+      let song = tracklist.eq(n);
+      if (song.hasClass('disabled')) {
+        let p = (index - 1) / tracklist.length;
+        loader.width(100 * p + '%').css('opacity', p);
+        tracklist.eq(n).removeClass('disabled');
+        index--;
+      }
+    }*/
+
+  /* controls */
 
   //stops playing the active song
-  stopBtn.click(function() {
-    videoBox.attr('height', videoH);
+  stopBtn.addEventListener('click', function() {
+    videoBox.style.height = videoH + 'px';
     setTimeout(function() {
-      activeSong.removeClass('active');
+      activeSong.classList.remove('active');
       activeSong = null;
       audio.pause();
-      audioBox.hide();
+      audio.style.display = "none";
     }, 10);
     closeBtn.click();
   });
 
   //opens and closes the lyrics pane
-  infoBtn.click(function() {
-    lyricsBox.addClass('active');
+  infoBtn.addEventListener('click', function() {
+    lyricsBox.classList.add('active');
   });
-  closeBtn.click(function() {
-    lyricsBox.removeClass('active');
+  closeBtn.addEventListener("click", function() {
+    lyricsBox.classList.remove('active');
   });
 
   /* events */
 
   //plays next song when the active one ends
   function audioEnded() {
-    let i = parseInt(activeSong.attr("index"));
+    let i = parseInt(activeSong.getAttribute("index"));
     if (i < tracklist.length) {
       tracklist.eq(i).click();
     } else {
